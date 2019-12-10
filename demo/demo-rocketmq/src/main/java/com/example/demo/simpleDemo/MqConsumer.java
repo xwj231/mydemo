f1cc2d922a78819abe79c71e1461b1d10d725051
xuj231 <1913859497@qq.com>
2019-12-10 15:28:24 +0800

    mq使用备注

--- demo/demo-rocketmq/src/main/java/com/example/demo/simpleDemo/MqConsumer.java
+++ demo/demo-rocketmq/src/main/java/com/example/demo/simpleDemo/MqConsumer.java
@@ -6,6 +6,7 @@ import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.message.Message;
 import org.apache.rocketmq.common.message.MessageExt;
@@ -22,12 +23,23 @@ public class MqConsumer {
     private static Logger logger = Logger.getLogger(MqConsumer.class);
 
     public static void main(String[] args) {
-        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("PushConsumer_yll");
-        consumer.setNamesrvAddr("192.168.2.144:9876");
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("PushConsumer_yl2");
+        consumer.setNamesrvAddr("192.168.2.148:9876");
         try {
             //可订阅多个tag，但是一个消息只能有一个tag *代表全部的Tag
-            consumer.subscribe("TopicA", "tagA||tagB");
-            
+            consumer.subscribe("TopicA", "tagA");
+
+            /**
+             *      1.消费策略是对新的customerGroup有效，
+             * 以前接收过消息的customerGroup，即使设置了消费策略，
+             * 还是从上一条消息开始消费。
+             *      2.订阅组(customerGroup)不存在情况下，
+             * 如果这个队列的消息最小Offset是0，则表示这个Topic上线时间不长，
+             * 服务器堆积的数据也不多，那么这个订阅组就从0开始消费。
+             * 
+             * 分析：
+             * http://www.itkeyword.com/doc/4394593802012908341/rocketMQ
+             */
             //消费策略(三种)
             //从队列最开始开始消费，即历史消息（还储存在broker的）全部消费一遍
             consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
@@ -35,16 +47,16 @@ public class MqConsumer {
             //consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
             //从某个时间点开始消费，和setConsumeTimestamp()配合使用，默认是半个小时以前
             //consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_TIMESTAMP);
-            //consumer.setConsumeTimestamp(String.valueOf(System.currentTimeMillis()));
+            //consumer.setConsumeTimestamp(UtilAll.timeMillisToHumanString3(System.currentTimeMillis()));
             
-            //设置消费模式：广播模式---还有一个负载均衡MessageModel.CLUSTERING
-//            consumer.setMessageModel(MessageModel.BROADCASTING);
+            //设置消费模式：广播模式MessageModel.BROADCASTIN---还有一个负载均衡MessageModel.CLUSTERING
+            consumer.setMessageModel(MessageModel.CLUSTERING);
             consumer.registerMessageListener(new MessageListenerConcurrently() {
                 @Override
                 public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
                     Message msg = list.get(0);
                     logger.info("消费消息："+msg.toString());
-                    System.out.println("消费消息："+msg.toString());
+                    System.out.println("消费消息："+new String(msg.getBody()));
                     return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
                 }
             });
--- demo/demo-rocketmq/src/main/java/com/example/demo/simpleDemo/MqProducer.java
+++ demo/demo-rocketmq/src/main/java/com/example/demo/simpleDemo/MqProducer.java
@@ -5,6 +5,8 @@ import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @author xuj231
  * @description
@@ -18,11 +20,12 @@ public class MqProducer {
         try {
             producer.start();
             logger.info("producer启动成功");
-            for (int i = 0; i < 5; i++) {
-                Message msg = new Message("TopicA", "tagA", "OrderID188", "Hello world".getBytes());
+            for (int i = 0; i < 100; i++) {
+                TimeUnit.SECONDS.sleep(10);
+                Message msg = new Message("TopicA", "tagA", "OrderID188", ("Hello world"+(i+1)).getBytes());
                 SendResult result = producer.send(msg);
                 logger.info("id：" + result.getMsgId() + " result:" + result.getSendStatus());
-                System.out.println("id：" + result.getMsgId() + " result:" + result.getSendStatus());
+                System.out.println("id：" + result.getMsgId() + " result:" + result.getSendStatus() + "第"+(i+1)+"条消息");
             }
         } catch (Exception e) {
             logger.error("发送消息失败，Exception error：" + e);
